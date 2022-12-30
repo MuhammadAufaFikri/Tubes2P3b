767fc2e027a9636aa98add74083ebd48323a5bf7
muhammadaufafikri <muhammadaufafikri@gmail.com>
2022-12-30 23:16:56 +0700

    change page declaration private

--- app/src/main/java/com/example/appunpar/MainActivity.java
+++ app/src/main/java/com/example/appunpar/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.appunpar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentResultListener;
 
 import android.os.Bundle;
@@ -10,16 +11,36 @@ import com.example.appunpar.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements FragmentResultListener {
     private ActivityMainBinding binding;
-    private loginFragment LoginFragment;
-    private homeFragment HomeFragment;
-    private pengumumanFragment PengumumanFragment;
-    private detailPengumumanFragment DetailPengumumanFragment;
-    private buatPengumumanfragment buatPengumumanfragment;
+    private FragmentManager fragmentManager;
+    private loginFragment login;
+    private homeFragment home;
+    private pengumumanFragment pengumuman;
+    private detailPengumumanFragment detailPengumuman;
+    private buatPengumumanfragment buatPengumuman;
+    private pertemuanFragment pertemuan;
+    private detailPertemuanFragment detailPertemuan;
+    private buatPertemuanFragment buatPertemuan;
+    private lihatJadwalFragment lihatJadwal;
+    private frsFragment frs;
+    private hitoryFRSfragment hitoryFRS;
+    private detailMatkulFragment detailMatkul;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.binding = ActivityMainBinding.inflate(this.getLayoutInflater());
+        setContentView(this.binding.getRoot());
+
+        this.login = loginFragment.newInstance("Fragment login");
+
+
+        this.fragmentManager = this.getSupportFragmentManager();
+
+        this.fragmentManager.beginTransaction().add(R.id.fragment_container, login).addToBackStack(null).commit();
+
+        this.fragmentManager.setFragmentResultListener("changePage", this, this);
+
+        this.fragmentManager.setFragmentResultListener("closeApplication", this, this);
     }
 
     @Override
