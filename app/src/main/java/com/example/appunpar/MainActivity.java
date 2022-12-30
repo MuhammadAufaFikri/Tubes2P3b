d4f3d04d155aca8d85320af99d850491514e7bfe
muhammadaufafikri <muhammadaufafikri@gmail.com>
2022-12-30 22:58:29 +0700

    list admin+ history raw

--- app/src/main/java/com/example/appunpar/MainActivity.java
+++ app/src/main/java/com/example/appunpar/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.appunpar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.os.Bundle;
 
 import com.example.appunpar.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
-    ActivityMainBinding binding;
+public class MainActivity extends AppCompatActivity implements FragmentResultListener {
+    private ActivityMainBinding binding;
+    private loginFragment LoginFragment;
+    private homeFragment HomeFragment;
+    private pengumumanFragment PengumumanFragment;
+    private detailPengumumanFragment DetailPengumumanFragment;
+    private buatPengumumanfragment buatPengumumanfragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+
     }
 }
