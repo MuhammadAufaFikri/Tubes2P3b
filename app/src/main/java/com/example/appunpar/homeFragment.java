d31fce0063bc7c38ef80e422adebde5ddaea0f9f
muhammadaufafikri <muhammadaufafikri@gmail.com>
2023-01-07 14:29:56 +0700

    pindah halaman

--- app/src/main/java/com/example/appunpar/homeFragment.java
+++ app/src/main/java/com/example/appunpar/homeFragment.java
@@ -8,7 +8,7 @@ import androidx.fragment.app.Fragment;
 
 import com.example.appunpar.databinding.FragmentHomeMahasiswaBinding;
 
-public class homeFragment  extends Fragment{
+public class homeFragment  extends Fragment implements View.OnClickListener{
     private FragmentHomeMahasiswaBinding binding;
     public static homeFragment newInstance(String title){
         homeFragment fragment = new homeFragment();
@@ -18,8 +18,20 @@ public class homeFragment  extends Fragment{
         return fragment;
     }
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
         this.binding = FragmentHomeMahasiswaBinding.inflate(inflater,container,false);
+        this.binding.keFramePengumuman.setOnClickListener(this);
         View view = this.binding.getRoot();
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(this.binding.keFramePengumuman== view){
+            Bundle result = new Bundle();
+            result.putInt("page",3);//pindah ke pengumuman
+            this.getParentFragmentManager().setFragmentResult("Fragment Pengumuman", result);
+            this.getParentFragmentManager().setFragmentResult("changePage",result);
+        }
     }
 }
