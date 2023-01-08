3034144e97f2e7b46218635cc512f215549ecd81
muhammadaufafikri <muhammadaufafikri@gmail.com>
2023-01-08 16:23:20 +0700

    deliver token in home

---
+++ app/src/main/java/com/example/appunpar/callVolleyPresent.java
@@ -0,0 +1,73 @@
+package com.example.appunpar;
+import android.content.Context;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class callVolleyPresent {
+    private static callVolleyPresent instance;
+    private RequestQueue requestQueue;
+    private static Context ctx;
+
+    private callVolleyPresent(Context context) {
+        ctx = context;
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized callVolleyPresent getInstance(Context context) {
+        if (instance == null) {
+            instance = new callVolleyPresent(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public static void callVolley(int method, String url, final JSONObject params, final String token, final VolleyCallback callback) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(method, url, params,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        callback.onSuccessResponse(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        callback.onErrorResponse(error);
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+        callVolleyPresent.getInstance(ctx).addToRequestQueue(jsonObjectRequest);
+    }
+
+    public interface VolleyCallback {
+        void onSuccessResponse(JSONObject result);
+        void onErrorResponse(VolleyError error);
+    }
+}
